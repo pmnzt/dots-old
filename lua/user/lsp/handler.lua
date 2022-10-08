be42fadf19a510f54cc937116890d52e4c0a0395
pmnzt <vponsve@gmail.com>
2022-10-08 07:15:45 -0400

    fixing lsp keymaps

--- lua/user/lsp/handler.lua
+++ lua/user/lsp/handler.lua
@@ -1,5 +1,3 @@
-local M = {}
-
 local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_cmp_ok then
   return
@@ -11,27 +9,6 @@ require('lspconfig')['tsserver'].setup {
 }
 
 local opts = { noremap = true, silent = true }
-
-
-local function lsp_buf_keymaps(bufnr)
-  local keymap = vim.api.nvim_buf_set_keymap
-  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-end
-
-local function lsp_keymaps()
-  local keymap = vim.api.nvim_set_keymap
-  keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
-end
-
-M.on_attach = function(client, bufnr)
-  lsp_buf_keymaps(bufnr)
-end
-
-lsp_keymaps()
-
-return M
+local keymap = vim.api.nvim_set_keymap
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
